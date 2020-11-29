7593cda0474cfb6409526efd08bbdb5217b91e9d
csabagyorfi <csaba@madetech.com>
2020-11-29 17:45:44 +0000

    Remove unncesarry things from starter

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/To-eat list/i);
   expect(linkElement).toBeInTheDocument();
 });
