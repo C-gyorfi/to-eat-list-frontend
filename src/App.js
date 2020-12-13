7a3f50a8f2511fe1e8d6799a6e69c699494e6e52
csabagyorfi <csaba@madetech.com>
2020-12-13 17:16:23 +0000

    Use the TEL API to render FoodList items
    This implementation uses the "https://cors-anywhere.herokuapp.com/" to
    proxy when making the request. Next step is removing this, deploying the
    frondend (maybe not heroku?) and implement
    the add item functionality

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ function App() {
       <header className="App-header">
         <h1>To-eat list</h1>
       </header>
-      <FoodList 
-        items={
-          [
-            {name: "Butter", expiryDate: new Date(2018, 11, 24).toISOString().split('T')[0]},
-            {name: "Bread", expiryDate: new Date(2018, 11, 26).toISOString().split('T')[0]},
-            {name: "Milk", expiryDate: new Date(2018, 11, 30).toISOString().split('T')[0]},
-          ]
-        }
-      />
+      <FoodList />
     </div>
   );
 }
--- src/FoodList/FoodList.js
+++ src/FoodList/FoodList.js
@@ -1,37 +1,66 @@
 import './FoodList.css';
+import React, { useEffect, useState } from "react";
 import ListItem from '../ListItem/ListItem';
-import PropTypes from 'prop-types';
 import { List } from 'antd';
 
+const FoodList = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-const FoodList = ({ items }) => {
-  const listItems = items.map(item => (
-    <ListItem
-      name={item.name}
-      expiryDate={item.expiryDate}
-    />)
-  );
-  return(
-    <List className="food-list-container"
-      size="large"
-      /* header={<div>Add new items</div>}
-      footer={<div>Add new items</div>} */
-      bordered
-      dataSource={listItems}
-      renderItem={item => (
-        <List.Item>
-          <List.Item.Meta
-            avatar={'✅'}
-            description={item}
-          />
-        </List.Item>
-      )}
-    />
-  )
+  useEffect(() => {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const apiurl = process.env.REACT_APP_TEL_API_URL;
+    const endpoint = "api/1/food_items/";
+
+    fetch(proxyurl + apiurl + endpoint)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    var listItems = {}
+    if (items.food_items) {
+      listItems = items.food_items.map(item => (
+        <ListItem
+          name={item.name}
+          expiryDate={item.expiry_date}
+        />)
+      );
+      return (
+        <List className="food-list-container"
+        size="large"
+        /* header={<div>Add new items</div>}
+        footer={<div>Add new items</div>} */
+        bordered
+        dataSource={listItems}
+        renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={'✅'}
+                description={item}
+              />
+            </List.Item>
+          )}
+        />
+      );
+    }
+    return <div>Something is not quite right...</div>;
+  }
 }
-
-FoodList.propTypes = { 
-  items: PropTypes.array.isRequired,
-};
 
 export default FoodList;
