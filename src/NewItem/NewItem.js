51baebc7f4ab769f186d4725d97dec64e5e53658
csabagyorfi <csaba@madetech.com>
2020-12-21 16:21:25 +0000

    Improve responsiveness for mobile use

--- src/NewItem/NewItem.js
+++ src/NewItem/NewItem.js
@@ -11,9 +11,9 @@ function addItem() {
 const NewItem = () => (
   <div className={`new-item-container`}>
     <Input.Group size="large">
-      <Input id='new-item-field' style={{ width: '50%' }} defaultValue="" />
-      <DatePicker id='new-expiry-date' size="large" style={{ width: '45%' }} />
-      <Button onClick={addItem} type="primary" size="large" style={{ width: '5%' }}>
+      <Input id='new-item-field' style={{ width: '45%' }} defaultValue="" />
+      <DatePicker id='new-expiry-date' size="large" style={{ width: '35%' }} />
+      <Button onClick={addItem} type="primary" size="large" style={{ width: '20%' }}>
           Add
       </Button>
     </Input.Group>
