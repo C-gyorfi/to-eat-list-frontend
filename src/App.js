8c890ada52c72503bf5eef5c0e1983cc5f1b52ac
csabagyorfi <csaba@madetech.com>
2020-12-22 11:17:18 +0000

    Update page title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>To-eat list</h1>
+        <h1>To Eat List 🦑🥒🍚🥢</h1>
       </header>
       <NewItem />
       <FoodList />
