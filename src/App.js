93d521e2af2c3db0dc04d78b8074f3eaa03fbd97
csabagyorfi <csaba@madetech.com>
2021-01-15 18:12:08 +0000

    Use cookies to store user login

--- src/App.js
+++ src/App.js
@@ -5,31 +5,33 @@ import 'antd/dist/antd.css';
 import NewItem from './NewItem/NewItem';
 import Login from './Auth/Login';
 import Logout from './Auth/Logout';
+import Cookies from 'universal-cookie';
 
 class App extends React.Component  {
   constructor(props) {
     super(props)
-    this.state = {loggedIn: false};
     this.handleLogin = this.handleLogin.bind(this)
     this.handleLogout = this.handleLogout.bind(this)
   }
 
-  handleLogin() {
-    this.setState({
-      loggedIn: true
-    })
-    console.log("LOGGEDIN:::", this.state.loggedIn)
+  handleLogin(response) {
+    const cookies = new Cookies();
+    let userEmail = response.profileObj.email;
+    // Temp fix to lock app from public 
+    if (process.env.REACT_APP_ALLOWED_USERS.split(" ").includes(userEmail)) {
+      cookies.set('user', userEmail, { path: '/' });
+      this.forceUpdate()
+    }
   }
 
   handleLogout() {
-    this.setState({
-      loggedIn: false
-    })
-    console.log("LOGGEDIN:::", this.state.loggedIn)
+    const cookies = new Cookies();
+    cookies.remove('user', { path: '/' });
+    this.forceUpdate()
   }
 
   render() {
-    if (this.state.loggedIn) {
+    if (new Cookies().get('user')) {
       return (
         <div className="App">
           <header className="App-header">
